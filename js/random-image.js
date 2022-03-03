c9c557e27860c1776f1bbb0d66de5d5403dd568c
ne52 <77370422+ne-52@users.noreply.github.com>
2022-03-03 17:06:05 +0900

    change_resorce

--- js/random-image.js
+++ js/random-image.js
@@ -1,4 +1,3 @@
-
 /*--------------------------------------------------------------------------
 	
 	Script Name : Random Image Script
@@ -10,29 +9,27 @@
 	
 --------------------------------------------------------------------------*/
 
-
 function random_image() {
-	
-	if ( document.getElementById( 'random-image' ) == null ) {
-		return;
-	}
-	
-	var image_ul   = document.getElementById( 'random-image' );
-	var image_li   = image_ul.getElementsByTagName( 'li' );
-	
-	var random_int = Math.floor( Math.random() * image_li.length );
-	
-	image_li[random_int].style.display = 'block';
-	
+  if (document.getElementById("random-image") == null) {
+    return;
+  }
+
+  var image_ul = document.getElementById("random-image");
+  var image_li = image_ul.getElementsByTagName("li");
+
+  var random_int = Math.floor(Math.random() * image_li.length);
+
+  image_li[random_int].style.display = "block";
 }
 
-
-
-
-if ( document.readyState == 'loading' ) {
-	document.addEventListener( 'DOMContentLoaded', function() {
-		random_image();
-	}, false );
+if (document.readyState == "loading") {
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      random_image();
+    },
+    false
+  );
 } else {
-	random_image();
+  random_image();
 }
